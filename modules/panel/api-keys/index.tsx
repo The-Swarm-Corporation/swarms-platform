1b6517669ee40b700408361cfee49b3aac2a7210
seville <amirrezasalimi0@gmail.com>
2024-03-09 13:17:26 +0330

    [CHORE] panel content

--- modules/panel/api-keys/index.tsx
+++ modules/panel/api-keys/index.tsx
@@ -1,10 +1,16 @@
 const ApiKeys = () => {
+  
   return (
-    <>
-      <h1 className="text-3xl font-extrabold text-white sm:text-4xl">
-        Api Keys
-      </h1>
-    </>
+    <div className="flex flex-col w-5/6">
+      <h1 className="text-3xl font-extrabold sm:text-4xl">API keys</h1>
+      <span className="mt-4 text-muted-foreground">
+        Your secret API keys are listed below. Please note that we do not
+        display your secret API keys again after you generate them. Do not share
+        your API key with others, or expose it in the browser or other
+        client-side code. In order to protect the security of your account,
+      </span>
+      <div className="mt-4"></div>
+    </div>
   );
 };
 
--- modules/panel/playground/index.tsx
+++ modules/panel/playground/index.tsx
@@ -1,10 +1,10 @@
 const Playground = () => {
   return (
-    <>
+    <div className="flex flex-col">
       <h1 className="text-3xl font-extrabold text-white sm:text-4xl">
         Playground
       </h1>
-    </>
+    </div>
   );
 };
 
