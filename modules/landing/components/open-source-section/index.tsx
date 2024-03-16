53256d0f39927eeb79c5cedf12698416979e771d
damirchi <damirchi@pro.ai>
2024-03-16 07:43:53 -0700

    [CHORE] make openSource's component files

---
+++ modules/landing/components/open-source-section/index.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+const OpenSourceSection = () => {
+    return(
+      <div></div>
+    )
+}
+
+export default OpenSourceSection;
--- modules/landing/index.tsx
+++ modules/landing/index.tsx
@@ -1,8 +1,10 @@
 import HeroSection from './components/hero-section';
+import OpenSourceSection from './components/open-source-section';
 export default function Landing() {
   return (
     <>
       <HeroSection />
+      <OpenSourceSection />
     </>
   );
 }
