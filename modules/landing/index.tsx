563b627526b9a9c4e4352f38171c3d20e11dca9f
damirchi <damirchi@pro.ai>
2024-03-17 16:43:17 -0700

    [FEAT] implement visualization section on landing

--- modules/landing/index.tsx
+++ modules/landing/index.tsx
@@ -2,13 +2,17 @@ import FeaturesSection from './components/features-section';
 import HeroSection from './components/hero-section';
 import OpenSourceSection from './components/open-source-section';
 import PanelSection from './components/panel-section';
+import PricingSection from './components/pricing-section';
+import { GoogleGeminiEffect } from './components/visualization-section';
 export default function Landing() {
   return (
     <>
       <HeroSection />
-      <OpenSourceSection />
+      <GoogleGeminiEffect />
+      <PricingSection />
       <PanelSection />
       <FeaturesSection />
+      <OpenSourceSection />
     </>
   );
 }
