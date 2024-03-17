e0a80e14a67b3a667841da5a3ce4422cf35cbedd
damirchi <damirchi@pro.ai>
2024-03-17 16:39:48 -0700

    [STYLE] update 3dCard layout

--- shared/components/3d-card/index.tsx
+++ shared/components/3d-card/index.tsx
@@ -84,7 +84,7 @@ export const CardBody = ({
   return (
     <div
       className={cn(
-        "h-96 w-96 [transform-style:preserve-3d]  [&>*]:[transform-style:preserve-3d]",
+        "h-96 flex-1 [transform-style:preserve-3d]  [&>*]:[transform-style:preserve-3d]",
         className
       )}
     >
