6adaa88facd8b61563c6b0c93f743bd849739d07
damirchi <damirchi@pro.ai>
2024-03-16 17:01:51 -0700

    [FEAT] implement panelSection component on home page

--- modules/landing/index.tsx
+++ modules/landing/index.tsx
@@ -1,10 +1,12 @@
 import HeroSection from './components/hero-section';
 import OpenSourceSection from './components/open-source-section';
+import PanelSection from './components/panel-section';
 export default function Landing() {
   return (
     <>
       <HeroSection />
       <OpenSourceSection />
+      <PanelSection />
     </>
   );
 }
