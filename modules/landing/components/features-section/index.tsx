80ee266f1d7efb5d2ee171328de87f774700cbd4
damirchi <damirchi@pro.ai>
2024-03-17 16:32:55 -0700

    [REFACTOR] update featuresSection's layout and update its contents

--- modules/landing/components/features-section/index.tsx
+++ modules/landing/components/features-section/index.tsx
@@ -10,25 +10,25 @@ import { motion } from "framer-motion";
 
 const items = [
     {
-        image: ImageScreen,
-        title: 'title title title',
-        description: 'description description description description'
+      image: ImageScreen,
+      title: 'Cheap',
+      description: 'Our affordable pricing ensures accessibility for all customers without compromising on quality. We prioritize cost-effectiveness to cater to budget-conscious clients while maintaining high standards of service and performance.'
     },
     {
         image: ImageScreen,
-        title: 'title title title',
-        description: 'description description description description'
+        title: 'Fast',
+        description: 'With our expedited service, you can expect rapid delivery and efficient turnaround times. We understand the importance of timely solutions and strive to meet deadlines promptly, enabling you to achieve your goals without unnecessary delays.'
     },
     {
         image: ImageScreen,
-        title: 'title title title',
-        description: 'description description description description'
+        title: 'Reliable',
+        description: 'Our commitment to reliability means you can trust us to consistently deliver exceptional results. We prioritize consistency and dependability in our services, ensuring that you can rely on us to meet your needs consistently and effectively.'
     }
 ]
 
 const FeaturesSection = () => {
   return (
-    <div className="flex flex-col container mx-auto py-16">
+    <div className="flex flex-col justify-center items-center container mx-auto py-16">
         <motion.div
           initial={{
             opacity: 0,
@@ -44,29 +44,28 @@ const FeaturesSection = () => {
           className="div"
         >
           <h2 className="text-center text-xl md:text-4xl font-bold text-black dark:text-white">
-            We sell soap worldwide
+            Our Benefits
           </h2>
           <p className="text-center text-base md:text-lg font-normal text-neutral-700 dark:text-neutral-200 max-w-md mt-2 mx-auto">
-            This globe is interactive and customizable. Have fun with it, and
-            don&apos;t forget to share it.
+           
           </p>
         </motion.div>
-        <div className="flex max-md:flex-col container mx-auto gap-4 py-8">
+        <div className="flex justify-center max-md:flex-col gap-4 py-8">
         {items.map(item => (
-            <Card3D className="inter-var">
-            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
+            <Card3D containerClassName="flex-1 max-w-sm" className="inter-var">
+            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto h-auto rounded-xl p-6 border">
               <CardItem
                 translateZ="50"
                 className="text-xl font-bold text-neutral-600 dark:text-white"
               >
-                Make things float in air
+                {item.title}
               </CardItem>
               <CardItem
                 as="p"
                 translateZ="60"
                 className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
               >
-                Hover over this card to unleash the power of CSS perspective
+                {item.description}
               </CardItem>
               <CardItem translateZ="100" className="w-full mt-4">
                 <Image
@@ -77,24 +76,7 @@ const FeaturesSection = () => {
                   alt="thumbnail"
                 />
               </CardItem>
-              <div className="flex justify-between items-center mt-20">
-                <CardItem
-                  translateZ={20}
-                  as={Link}
-                  href="https://twitter.com/mannupaaji"
-                  target="__blank"
-                  className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-                >
-                  Try now →
-                </CardItem>
-                <CardItem
-                  translateZ={20}
-                  as="button"
-                  className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-                >
-                  Sign up
-                </CardItem>
-              </div>
+  
             </CardBody>
           </Card3D>
         ))}
