b0854fe64ed9441f7a7a3509c84851d6abe173b3
damirchi <damirchi@pro.ai>
2024-03-22 13:50:27 -0700

    [FEAT] implement codeBox component and make it dynamic

--- shared/components/code-box/index.tsx
+++ shared/components/code-box/index.tsx
@@ -1,23 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ReactPrismjs from "@uiw/react-prismjs";
 import 'prismjs/themes/prism-tomorrow.css';
+import { cn } from "@/shared/utils/cn";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/shared/components/ui/select';
 
 interface CodeBoxItems {
-    title?: string;
-    sourceCode: string;
+  title?: string;
+  sourceCode: string;
 }
 
-interface CodeBox {
-    sampleCodes: Record<string, CodeBoxItems>;
-    language: string;
+interface ICodeBoxProps {
+  initLanguage?: string;
+  sampleCodes: Record<string, CodeBoxItems>;
+  className?: string;
 }
 
-export default function Code({ language, sampleCodes }: CodeBox) {
-
+const CodeBox = ({ sampleCodes, initLanguage }: ICodeBoxProps) => {
+  const [selectedLanguage, setSelectedLanguage] = useState(initLanguage || Object.keys(sampleCodes)[0]);
 
   return (
-    <>
-        <ReactPrismjs prefixCls="prism" language={language} source="console.log('Hello Wold!')" />
-    </>
+    <div className={cn("rounded-lg bg-slate-900")}>
+      <div className="p-2 flex justify-center items-center">
+        <p className="flex-1">
+          {sampleCodes[selectedLanguage].title}
+        </p>
+        {Object.keys(sampleCodes).length > 1 &&
+          <div className="w-fit">
+          <Select onValueChange={setSelectedLanguage} value={selectedLanguage}>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="model" />
+            </SelectTrigger>
+            <SelectContent>
+              {Object.keys(sampleCodes).map((language, index) => (
+                <SelectItem key={`SELECT_ITEM_${language}_${index}`} value={language}>
+                  {language}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+        }
+      </div>
+      <ReactPrismjs className="!m-0 !bg-[rgb(22 24 29)] rounded-b-lg" prefixCls="prism" language={selectedLanguage} source="console.log('Hello Wold!')" />
+    </div>
   );
 }
+
+export default CodeBox;
