427e16d4dde5442ee2851db9b37f7f01c9fea305
seville <amirrezasalimi0@gmail.com>
2024-03-26 00:56:12 +0330

    [CHORE] codebox

--- shared/components/code-box/index.tsx
+++ shared/components/code-box/index.tsx
@@ -1,5 +1,5 @@
-'use client'
-import { useState } from 'react';
+'use client';
+import { useEffect, useState } from 'react';
 import ReactPrismjs from '@uiw/react-prismjs';
 import '@/shared/styles/prism-one-dark.css';
 import 'prismjs/components/prism-python';
@@ -23,6 +23,7 @@ export interface ICodeBoxItems {
 export type ISampleCodes = Record<string, ICodeBoxItems>;
 
 interface ICodeBoxProps {
+  value?: string;
   initLanguage?: string;
   sampleCodes: ISampleCodes;
   classes?: {
@@ -30,41 +31,57 @@ interface ICodeBoxProps {
     title?: string;
     content?: string;
   };
+  hideList?: boolean;
 }
 
-const CodeBox = ({ sampleCodes, initLanguage, classes }: ICodeBoxProps) => {
+const CodeBox = ({
+  sampleCodes,
+  initLanguage,
+  classes,
+  value,
+  hideList
+}: ICodeBoxProps) => {
   const [selectedLanguage, setSelectedLanguage] = useState(
     initLanguage || Object.keys(sampleCodes)[0]
   );
+  useEffect(() => {
+    if (value) {
+      setSelectedLanguage(value);
+    }
+  }, [value]);
 
   return (
     <div className={cn('rounded-xl bg-slate-900', classes?.root)}>
       <div
         className={cn('p-2 flex justify-center items-center', classes?.title)}
       >
-        <p className="flex-1 text-start">{sampleCodes[selectedLanguage].title}</p>
-        {Object.keys(sampleCodes).length > 1 && (
-          <div className="w-fit">
-            <Select
-              onValueChange={setSelectedLanguage}
-              value={selectedLanguage}
-            >
-              <SelectTrigger className="w-full">
-                <SelectValue placeholder="model" />
-              </SelectTrigger>
-              <SelectContent>
-                {Object.keys(sampleCodes).map((language, index) => (
-                  <SelectItem
-                    key={`SELECT_ITEM_${language}_${index}`}
-                    value={language}
-                  >
-                    {language}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-          </div>
-        )}
+        <p className="flex-1 text-start">
+          {sampleCodes[selectedLanguage].title}
+        </p>
+        {hideList
+          ? null
+          : Object.keys(sampleCodes).length > 1 && (
+              <div className="w-fit">
+                <Select
+                  onValueChange={setSelectedLanguage}
+                  value={selectedLanguage}
+                >
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="model" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {Object.keys(sampleCodes).map((language, index) => (
+                      <SelectItem
+                        key={`SELECT_ITEM_${language}_${index}`}
+                        value={language}
+                      >
+                        {language}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </div>
+            )}
       </div>
       <ReactPrismjs
         className={cn(
