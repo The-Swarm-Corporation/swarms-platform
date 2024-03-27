c3c0184803b405ed9a0b183f8739feb59efbcfe1
amird308 <amird308@gmail.com>
2024-03-27 08:51:14 +0330

    [CHORE] change order of hero's section text

--- modules/landing/components/hero-section/index.tsx
+++ modules/landing/components/hero-section/index.tsx
@@ -6,21 +6,20 @@ import { cn } from '@/shared/utils/cn';
 import { Github } from 'lucide-react';
 import Link from 'next/link';
 import WavyBackground from '@/shared/components/wavy-background';
-import CodeBox from '@/shared/components/code-box';
 
 const HeroSection = () => {
   return (
     <WavyBackground className="mx-auto md:pb-40 w-full">
       <div className="flex flex-col items-center px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8 gap-6 text-center">
-        <h1 className="text-6xl md:text-7xl font-bold text-primary">Swarms</h1>
         <h1 className="text-6xl md:text-7xl font-bold ">
           Orchestrate Agents with
         </h1>
+        <h1 className="text-6xl md:text-7xl font-bold text-primary">Swarms</h1>
         <span className="text-2xl font-medium">
           Production-Grade Agents Through Multi-Agent Collaboration
         </span>
         <div className="flex flex-col bg-background rounded-xl overflow-hidden">
-          <span className="w-full bg-blue-950 text-white p-1">Terminal</span>
+          <span className="w-full bg-primary text-white p-1">Terminal</span>
           <span className="p-3 text-white">pip3 install -U swarms</span>
         </div>
         <div className="flex gap-4 mt-8">
