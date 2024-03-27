e45bfcf1677394a87fb6a408b67016ee67bfa33c
seville <amirrezasalimi0@gmail.com>
2024-03-27 07:43:29 +0330

    [CHORE] links

--- shared/constants/links.ts
+++ shared/constants/links.ts
@@ -3,10 +3,11 @@ export const PLATFORM = {
   API_KEYS: '/platform/api-keys',
   USAGE: '/platform/usage',
   ACCOUNT: '/platform/account',
-  PLAYGROUND: '/platform/playground',
+  PLAYGROUND: '/platform/playground'
 };
-export const AUTH={
-  CHANGE_PASSWORD: "/signin/update_password",
-}
+export const AUTH = {
+  CHANGE_PASSWORD: '/signin/update_password'
+};
 export const SWARMS_GITHUB = 'https://github.com/kyegomez/swarms';
 export const DISCORD = 'https://discord.gg/gRXy5mpFHz';
+export const SWARM_CALENDLY = 'https://calendly.com/swarm-corp/30min';
