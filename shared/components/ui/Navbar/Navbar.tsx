f1930614f7beef8585e9ab0da0c278b93d93a75d
damirchi <damirchi@pro.ai>
2024-04-01 17:19:58 -0700

    [FEAT] implement mobile version of header

--- shared/components/ui/Navbar/Navbar.tsx
+++ shared/components/ui/Navbar/Navbar.tsx
@@ -14,7 +14,7 @@ export default async function Navbar() {
       <a href="#skip" className="sr-only focus:not-sr-only">
         Skip to content
       </a>
-      <div className="max-w-6xl px-6 mx-auto">
+      <div className="max-w-6xl mx-auto">
         <Navlinks user={user} />
       </div>
     </nav>
--- shared/components/ui/Navbar/Navlinks.tsx
+++ shared/components/ui/Navbar/Navlinks.tsx
@@ -13,8 +13,10 @@ import {
   Drawer,
   DrawerContent,
   DrawerTrigger,
+  DrawerClose
 } from "../drawer";
 import { Button } from '../Button';
+import { Menu, SquareX } from 'lucide-react';
 
 interface NavlinksProps {
   user?: any;
@@ -25,11 +27,12 @@ export default function Navlinks({ user }: NavlinksProps) {
 
   return (
     <div className="relative flex flex-row justify-between py-4 align-center md:py-6">
-      <div className="flex flex-shrink-0 items-center ">
-        <div className="flex items-center w-[40px] h-[40px] min-w-[40px]">
+      <div className="flex flex-shrink-0 items-center">
+        {/* desktop */}
+        <div className="flex items-center w-[40px] h-[40px] min-w-[40px] max-md:hidden">
           <Logo />
         </div>
-        <nav className="flex ml-2 md:ml-6 gap-3">
+        <nav className="flex ml-2 md:ml-6 gap-3 max-md:hidden">
           <Link href="/pricing" className={s.link}>
             Pricing
           </Link>
@@ -48,7 +51,47 @@ export default function Navlinks({ user }: NavlinksProps) {
             </Link>
           )}
         </nav>
+        {/* mobile */}
+        <div className="md:hidden">
+          <Drawer direction="left">
+            <DrawerTrigger asChild>
+              <Button className="text-white" variant="link"><Menu /></Button>
+            </DrawerTrigger>
+
+            <DrawerContent className="flex flex-col h-full w-[300px] mt-24 fixed bottom-0 rounded-none">
+              <div className="p-4 bg-background flex-1 h-full flex flex-col gap-4">
+                <div className='flex gap-2 items-center'>
+                <div className="flex items-center w-[40px] h-[40px] min-w-[40px]">
+                  <Logo />
+                </div>
+                <h2 className='font-bold text-primary'>SWARMS</h2>
+                </div>
+                <DrawerClose className='absolute top-4 right-4'>  
+                  <SquareX />
+                </DrawerClose>
+                <Link href="/pricing" className={s.link}>
+                  Pricing
+                </Link>
+                <Link href={SWARMS_GITHUB} className={s.link}>
+                  GitHub
+                </Link>
+                <Link href={DISCORD} className={s.link}>
+                  Community
+                </Link>
+                <Link href="https://swarms.apac.ai/en/latest/" className={s.link}>
+                  Docs
+                </Link>
+                {user && (
+                  <Link href={PLATFORM.DASHBOARD} className={s.link}>
+                    Dashboard
+                  </Link>
+                )}
+              </div>
+            </DrawerContent>
+          </Drawer>
+        </div>
       </div>
+      {/* common */}
       <div className="flex justify-end items-center space-x-8 w-full">
         {user ? (
           <form onSubmit={(e) => handleRequest(e, SignOut, router)}>
@@ -66,17 +109,6 @@ export default function Navlinks({ user }: NavlinksProps) {
           Get Demo
         </Link>
       </div>
-
-      <Drawer direction="right">
-      <DrawerTrigger asChild>
-        <Button>Open Drawer</Button>
-      </DrawerTrigger>
-        <DrawerContent className="flex flex-col h-full w-[400px] mt-24 fixed bottom-0 rounded-none">
-          <div className="p-4 bg-white flex-1 h-full">
-            
-          </div>
-        </DrawerContent>
-      </Drawer>
     </div>
   );
 }
