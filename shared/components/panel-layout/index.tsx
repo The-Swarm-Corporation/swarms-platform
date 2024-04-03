8f2b2657ac3b33db1f09224280791fd61ded6645
seville <amirrezasalimi0@gmail.com>
2024-04-04 00:32:23 +0330

    [FEAT] panel responsive menu

--- shared/components/panel-layout/index.tsx
+++ shared/components/panel-layout/index.tsx
@@ -1,5 +1,5 @@
 'use client';
-import PanelLayoutSidebar from './sidebar';
+import PanelLayoutSidebar from './components/sidebar';
 import { ThemeProvider } from '../ui/theme-provider';
 import { TrpcProvider } from '@/shared/utils/trpc/trpc-provider';
 import BasicOnboardingModal from '../basic-onboarding-modal';
@@ -14,13 +14,12 @@ const PanelLayout = ({ children }: { children: React.ReactNode }) => {
         disableTransitionOnChange
       >
         <TrpcProvider>
-          <div className="flex flex-row w-screen h-screen">
+          <div className="flex flex-row w-screen h-screen max-md:flex-col">
             {/* sidebar */}
-            <div className="flex flex-col flex-shrink-0 w-[250px] h-screen border-r border-gray-900">
-              <PanelLayoutSidebar />
-            </div>
+            <PanelLayoutSidebar />
+
             {/* content */}
-            <div className="flex container h-full overflow-scroll mx-auto py-8">
+            <div className="flex container h-full overflow-scroll mx-auto py-8 max-md:z-10 max-md:pt-16">
               {children}
             </div>
           </div>
