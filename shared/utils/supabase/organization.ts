73083749ae380ad91d344f442175c8a560de892d
seville <amirrezasalimi0@gmail.com>
2024-04-14 05:35:01 +0330

    [FEAT] invitation

--- shared/utils/supabase/organization.ts
+++ shared/utils/supabase/organization.ts
@@ -50,21 +50,21 @@ export const submitInviteCode = async (code: string, userId: string) => {
 
   // check user is already member
   const orgId = invite.data?.[0].organization_id ?? '';
-  const userRole = await getUserOrganizationRole(orgId, userId);
+  const isMember = await supabaseAdmin
+    .from('swarms_cloud_organization_members')
+    .select('*')
+    .eq('user_id', userId)
+    .eq('organization_id', orgId)
+    .neq('is_deleted', true)
+    .limit(1);
 
-  if (userRole) {
+  if (isMember.data?.length) {
     throw new Error('Already a member');
   }
 
   if (invite.data?.length) {
-    const inviteData = invite.data[0];
-    // check if its cancelled
-    if (inviteData.status === 'canceled') {
-      throw new Error('Invite cancelled');
-    }
-
     // check its not expired
-
+    const inviteData = invite.data[0];
     const now = new Date().getTime();
     const inviteTime = new Date(inviteData.created_at).getTime();
     const diff = (now - inviteTime) / 1000;
