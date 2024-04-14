690579d4d2135c817f5f970c42c60867ef3407dd
gilbertoaceville <gilbertoaceville@gmail.com>
2024-04-14 16:42:50 +0100

    feat: organization feature additions continues

--- app/(panel)/platform/organization/page.tsx
+++ app/(panel)/platform/organization/page.tsx
@@ -1,1 +1,12 @@
-export { default } from "@/modules/platform/organization";
+import { createClient } from '@/shared/utils/supabase/server';
+import Organization from '@/modules/platform/organization';
+
+export default async function OrganizationPage() {
+  const supabase = createClient();
+
+  const {
+    data: { user }
+  } = await supabase.auth.getUser();
+
+  return <Organization {...{ user }} />;
+}
--- modules/platform/organization/index.tsx
+++ modules/platform/organization/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import React, { useState } from 'react';
-import { Plus, PlusCircle } from 'lucide-react';
+import React, { useMemo, useState } from 'react';
+import { Cross, Plus, PlusCircle, X } from 'lucide-react';
 import {
   Select,
   SelectContent,
@@ -20,20 +20,57 @@ import {
 import { Button } from '@/shared/components/ui/Button';
 import Input from '@/shared/components/ui/Input';
 
-export const roles = [
+const roles = [
   { label: 'List Team roles', value: 'Team roles' },
   { label: 'Owner', value: 'owner' },
   { label: 'Manager', value: 'manager' },
   { label: 'Reader', value: 'reader' }
 ] as const;
-export const inviteRoles = roles.slice(1);
-export default function Organization() {
+const inviteRoles = roles.slice(1);
+export const team = [
+  { email: 'gilbertoaceville@gmail.com', role: 'owner' },
+  { email: 'sammyfall@gmail.com', role: 'manager' },
+  { email: 'tawnytray@gmail.com', role: 'reader' },
+  { email: 'lebronguana@gmail.com', role: 'reader' },
+  { email: 'diaznunez@gmail.com', role: 'owner' }
+];
+export default function Organization({ user }: { user: any }) {
+  console.log({ user });
   const [organizationName, setOrganizationName] = useState('');
   const [email, setEmail] = useState('');
   const [filterRole, setFilterRole] = useState<string>(roles[0]?.value);
   const [inviteRole, setInviteRole] = useState<string>(
     roles[roles.length - 1]?.value
   );
+  const [invites, setInvites] = useState([{ role: 'reader' }]);
+  const [userRole, setUserRole] = useState('Reader');
+
+  const allUserRoles = useMemo(
+    () =>
+      roles
+        .filter((role) => role.value !== 'Team roles')
+        .map((role) => role.value),
+    []
+  );
+
+  console.log({ allUserRoles });
+
+  const isMoreInvites = invites.length > 1;
+  const inviteButtonText = isMoreInvites ? 'Invite All' : 'Invite';
+
+  function addMoreInvites() {
+    setInvites((prevState) => [...prevState, { role: 'reader' }]);
+  }
+
+  function removeInvites(index: number) {
+    setInvites((prevState) => {
+      return prevState.filter((_, i) => i !== index);
+    });
+  }
+
+  function changeUserRole(role: string) {
+    setUserRole(role);
+  }
 
   return (
     <section className="w-full">
@@ -106,50 +143,72 @@ export default function Organization() {
               <DialogHeader>
                 <DialogTitle>Invite members by email address</DialogTitle>
               </DialogHeader>
-              <form>
-                <div className="flex items-center gap-2 w-full my-3">
-                  <div className="w-full">
-                    <label htmlFor="email" className="text-right">
-                      Email
-                    </label>
-                    <Input
-                      id="email"
-                      type="email"
-                      value={email}
-                      className="my-2 w-full"
-                      placeholder="swarms@example.com"
-                      onChange={(value) => {
-                        setEmail(value);
-                      }}
-                    />
-                  </div>
-                  <div className="w-2/4">
-                    <label className="text-right">Role</label>
-
-                    <Select
-                      onValueChange={(value) => {
-                        setInviteRole(value);
-                      }}
-                      value={inviteRole}
-                    >
-                      <SelectTrigger className="w-full">
-                        <SelectValue placeholder={inviteRole} />
-                      </SelectTrigger>
-                      <SelectContent>
-                        {inviteRoles?.map((role) => (
-                          <SelectItem key={role.label} value={role.value}>
-                            {role.label}
-                          </SelectItem>
-                        ))}
-                      </SelectContent>
-                    </Select>
-                  </div>
+              <form onSubmit={(e) => e.preventDefault()}>
+                <div className="flex gap-2 items-center mt-3 w-full">
+                  <p className="w-full">Email</p>
+                  <p className="w-2/4">Role</p>
                 </div>
-                <DialogFooter className="sm:justify-between">
-                  <Button type="submit" variant="outline" className="gap-2">
+                {invites.map((invite, index) => (
+                  <div
+                    key={`${invite.role}-${index}`}
+                    className="flex items-center gap-2 w-full"
+                  >
+                    <div className="w-full">
+                      <label htmlFor="email" className="text-right hidden">
+                        Email
+                      </label>
+                      <Input
+                        id="email"
+                        type="email"
+                        value={email}
+                        className="my-2 w-full"
+                        placeholder="swarms@example.com"
+                        onChange={(value) => {
+                          setEmail(value);
+                        }}
+                      />
+                    </div>
+                    <div className="w-2/4">
+                      <Select
+                        onValueChange={(value) => {
+                          setInviteRole(value);
+                        }}
+                        value={inviteRole}
+                      >
+                        <SelectTrigger className="w-full">
+                          <SelectValue placeholder={invite.role} />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {inviteRoles?.map((role) => (
+                            <SelectItem key={role.label} value={role.value}>
+                              {role.label}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    </div>
+
+                    {isMoreInvites && (
+                      <X
+                        size={20}
+                        className="text-primary cursor-pointer"
+                        onClick={() => removeInvites(index)}
+                      />
+                    )}
+                  </div>
+                ))}
+                <DialogFooter className="sm:justify-between mt-3">
+                  <Button
+                    type="button"
+                    variant="outline"
+                    className="gap-2"
+                    onClick={addMoreInvites}
+                  >
                     <PlusCircle size={15} /> Add more
                   </Button>
-                  <Button type="submit" className="w-1/3">Invite</Button>
+                  <Button type="submit" className="w-1/3">
+                    {inviteButtonText}
+                  </Button>
                 </DialogFooter>
               </form>
             </DialogContent>
@@ -178,7 +237,28 @@ export default function Organization() {
           </Select>
         </div>
 
-        <div className="flex flex-col items-center justify-center border rounded-md px-4 py-8 text-card-foreground cursor-pointer hover:opacity-80"></div>
+        <div className="flex justify-between border rounded-md px-4 py-8 text-card-foreground cursor-pointer hover:opacity-80">
+          <div className="flex items-center gap-2">
+            <span className="w-10 h-10 flex justify-center items-center bg-slate-500 text-white rounded-full uppercase">
+              {team[0].email.charAt(0)}
+            </span>
+            <p>{team[0].email}</p>
+          </div>
+          <div className="relative">
+            <div className="border w-28 py-1 text-center rounded-md capitalize">{userRole}</div>
+            <ul className="absolute list-none border bg-secondary w-32 flex flex-col items-center rounded-md bottom-8 -right-14">
+              {allUserRoles.map((role) => (
+                <li
+                  onClick={() => setUserRole(role)}
+                  className="hover:text-secondary hover:bg-foreground capitalize w-full py-2 text-center"
+                >
+                  {role}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <Button>Leave</Button>
+        </div>
       </div>
     </section>
   );
