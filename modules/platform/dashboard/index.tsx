f2ced6ab3ec919823b9b6b5e016beabe214cd2f2
gilbertoaceville <gilbertoaceville@gmail.com>
2024-04-26 15:25:50 +0100

    fix: dashboard mobile layout

--- modules/platform/dashboard/index.tsx
+++ modules/platform/dashboard/index.tsx
@@ -130,8 +130,8 @@ const Dashboard = () => {
             Subscribe
           </Button>
         </div>
-        <div className="flex gap-4">
-          <div className="w-1/2 flex flex-col gap-2 border rounded-md p-4">
+        <div className="flex gap-4 max-sm:flex-wrap">
+          <div className="sm:w-1/2 flex flex-col gap-2 border rounded-md p-4">
             <h2 className="text-2xl font-bold">Create an Organization</h2>
             <span className="text-muted-foreground">
               Create an organization to invite your team members and manage your projects.
@@ -142,7 +142,7 @@ const Dashboard = () => {
               </Button>
             </Link>
           </div>
-          <div className="w-1/2 flex flex-col gap-2 border rounded-md p-4">
+          <div className="sm:w-1/2 flex flex-col gap-2 border rounded-md p-4">
             <h2 className="text-2xl font-bold">Add Team Members</h2>
             <span className="text-muted-foreground">
               Invite your team members to collaborate on projects.
@@ -154,8 +154,8 @@ const Dashboard = () => {
             </Link>
           </div>
         </div>
-        <div className="flex gap-4">
-          <div className="w-1/2 flex flex-col gap-2 border rounded-md p-4">
+        <div className="flex gap-4 max-sm:flex-wrap">
+          <div className="sm:w-1/2 flex flex-col gap-2 border rounded-md p-4">
             <h2 className="text-2xl font-bold">API Keys</h2>
             <span className="text-muted-foreground">
               Create now an API key to access an API Key.
@@ -166,7 +166,7 @@ const Dashboard = () => {
               </Button>
             </Link>
           </div>
-          <div className="w-1/2 flex flex-col gap-2 border rounded-md p-4">
+          <div className="sm:w-1/2 flex flex-col gap-2 border rounded-md p-4">
             <h2 className="text-2xl font-bold">Documention</h2>
             <span className="text-muted-foreground">
               Learn more about Swarms through our documentation.
