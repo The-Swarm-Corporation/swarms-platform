6fd2b2b9da499628caf93b65cc518cbc6343b13f
gilbertoaceville <gilbertoaceville@gmail.com>
2024-04-26 04:00:59 +0100

    feat: redirect authenticated users to explorer page

---
+++ shared/utils/stack-middlewares/stack.ts
@@ -0,0 +1,19 @@
+import { NextMiddleware, NextResponse } from 'next/server';
+import { MiddlewareFactory } from './types';
+
+/**
+ * @param functions refers to middleware functions to be chained together
+ * @param index current index of the middleware
+ * @returns the next middleware function after the current middleware
+ */
+export function stackMiddlewares(
+  functions: MiddlewareFactory[] = [],
+  index = 0
+): NextMiddleware {
+  const current = functions[index];
+  if (current) {
+    const next = stackMiddlewares(functions, index + 1);
+    return current(next);
+  }
+  return () => NextResponse.next();
+}
