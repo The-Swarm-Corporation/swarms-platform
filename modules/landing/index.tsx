9c79fd54e14ab9d6295664d568810bc7415f46de
Eternal Reclaimer <98760976+kyegomez@users.noreply.github.com>
2024-05-21 00:49:27 -0400

    Update index.tsx

--- modules/landing/index.tsx
+++ modules/landing/index.tsx
@@ -13,7 +13,6 @@ export default function Landing() {
       <CompaniesSection />
       <CodeSampleSection />
       <GoogleGeminiEffect />
-      <PanelSection />
       <PricingSection />
       <OpenSourceSection />
     </>
