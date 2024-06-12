7a19bdd9b2fdf309c7d0c530b59973530a5e0fdb
tunki1201 <tunki1201@gmail.com>
2024-06-12 04:47:12 -0400

    BUG/fix-modelcard-reactmarkdown

--- modules/model/components/markdown-preview.tsx
+++ modules/model/components/markdown-preview.tsx
@@ -2,7 +2,7 @@
 import ReactMarkdownPreview from '@uiw/react-markdown-preview';
 
 const MarkdownPreview = ({ content }: { content: string }) => {
-  return <ReactMarkdownPreview className="!bg-transparent" source={content} />;
+  return <ReactMarkdownPreview className="!bg-transparent !text-white" source={content} />;
 };
 
 export default MarkdownPreview;
