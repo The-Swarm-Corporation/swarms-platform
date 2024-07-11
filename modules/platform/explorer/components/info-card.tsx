d931c3daa6fb89a8232d6f36d204df3df3734dac
gilbertoaceville <gilbertoaceville@gmail.com>
2024-07-11 08:13:01 +0100

    fix: rating placement on explorer cards

--- modules/platform/explorer/components/info-card.tsx
+++ modules/platform/explorer/components/info-card.tsx
@@ -65,8 +65,19 @@ const InfoCard = ({
         className,
       )}
     >
-      <div className="flex items-center justify-center h-10 bg-primary text-white rounded-lg aspect-square">
-        {icon}
+      <div>
+        <div className="flex items-center justify-center h-10 bg-primary text-white rounded-lg aspect-square">
+          {icon}
+        </div>
+
+        {id && (
+          <div className="mt-3 relative flex items-center justify-center gap-1 xl:hidden">
+            <div className="mb-0.5">
+              <ReactStars value={modelRating} isEdit={false} count={1} />
+            </div>
+            <span className="text-xs font-semibold">{modelRating || 0}/5</span>
+          </div>
+        )}
       </div>
       <div className="h-4/5 flex flex-col overflow-y-auto no-scrollbar">
         <div className="flex flex-col gap-2 flex-grow">
@@ -87,28 +98,13 @@ const InfoCard = ({
       </div>
 
       {id && (
-        <div className="hover:opacity-70">
-          <svg
-            width="95"
-            height="25"
-            viewBox="0 0 95 25"
-            xmlns="http://www.w3.org/2000/svg"
-            className="rating-svg absolute left-0 bottom-0 scale-x-[2.5] scale-y-[1.8]"
-          >
-            <path d="M21 0H95V25H0L21 0Z" />
-          </svg>
-          <div className="absolute -left-4 bottom-0.5 text-white px-4 py-1">
-            <div className="relative flex items-center justify-center gap-1 w-[80px]">
-              <div className="mb-0.5">
-                <ReactStars value={modelRating} isEdit={false} count={1} />
-              </div>
-              <span className="text-sm font-semibold">
-                {modelRating || 0}/5
-              </span>
+          <div className="bottom-2 left-4 absolute items-center justify-center gap-1 hidden xl:flex">
+            <div className="mb-0.5">
+              <ReactStars value={modelRating} isEdit={false} count={1} />
             </div>
+            <span className="text-sm font-semibold">{modelRating || 0}/5</span>
           </div>
-        </div>
-      )}
+        )}
 
       <div
         className="cursor-pointer hover:opacity-70"
@@ -119,12 +115,12 @@ const InfoCard = ({
           height="25"
           viewBox="0 0 95 25"
           xmlns="http://www.w3.org/2000/svg"
-          className="rating-svg absolute right-[150px] bottom-0 scale-x-[2.5] scale-y-[1.8] fill-[#FB0101]"
+          className="rating-svg absolute right-[150px] xl:right-[120px] bottom-0 scale-x-[2.5] scale-y-[1.8] fill-[#FB0101]"
         >
           <path d="M21 0H95V25H0L21 0Z" className="fill-[#b42020]" />
         </svg>
         <div className="absolute right-[150px] bottom-0 text-white px-4 py-1">
-          <div className="relative flex items-center justify-center gap-2 w-[80px]">
+          <div className="relative flex items-center justify-center gap-2 w-[80px] xl:w-[66px]">
             <span>Share</span>
             <Share2 />
           </div>
