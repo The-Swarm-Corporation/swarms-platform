7b81b8e5ac5e5a4da13b7c6e9decbda265bd509d
gilbertoaceville <gilbertoaceville@gmail.com>
2025-02-05 22:25:58 +0100

    chore: change supabase anon key

--- shared/utils/supabase/server.ts
+++ shared/utils/supabase/server.ts
@@ -9,7 +9,7 @@ export const createClient = async () => {
 
   return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_ANON_KEY!,
 
     {
       cookies: {
