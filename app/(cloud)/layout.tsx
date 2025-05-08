b1d2249bb57cf8ac8dc15cb92b414e7827cd76d9
gilbertoaceville <gilbertoaceville@gmail.com>
2025-05-08 13:44:53 +0100

    feat: setup api key provider

--- app/(cloud)/layout.tsx
+++ app/(cloud)/layout.tsx
@@ -2,6 +2,7 @@ import type React from 'react';
 import '@/shared/styles/main.css';
 import { Header } from '@/shared/components/telemetry/header';
 import { PlatformTabs } from '@/shared/components/telemetry/platform-tabs';
+import { APIkeyProvider } from '@/shared/components/ui/apikey.provider';
 
 export default function RootLayout({
   children,
@@ -11,10 +12,13 @@ export default function RootLayout({
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1 container mx-auto px-4 py-6">
-        <PlatformTabs />
-        {children}
-      </main>
+      
+      <APIkeyProvider>
+        <main className="flex-1 container mx-auto px-4 py-6">
+          <PlatformTabs />
+          {children}
+        </main>
+      </APIkeyProvider>
     </div>
   );
 }
---
+++ shared/components/ui/apikey.provider.tsx
@@ -0,0 +1,93 @@
+'use client';
+
+import { trpc } from '@/shared/utils/trpc/trpc';
+import {
+  createContext,
+  useContext,
+  PropsWithChildren,
+  useRef,
+  useState,
+  useEffect,
+  RefObject,
+} from 'react';
+
+interface APIkeyProviderProps extends PropsWithChildren {
+  isShareId?: boolean;
+  isCreateAutoKey?: boolean
+}
+interface APIContextType {
+  isInitializing: boolean;
+  creationError: string | null;
+  isCreatingApiKey: RefObject<boolean>;
+  apiKey: string;
+  isApiKeyLoading: boolean;
+}
+
+const APIContext = createContext<APIContextType | undefined>(undefined);
+
+export const APIkeyProvider = ({
+  children,
+  isShareId = false,
+  isCreateAutoKey = false
+}: APIkeyProviderProps) => {
+  const [isInitializing, setIsInitializing] = useState(true);
+  const [creationError, setCreationError] = useState<string | null>(null);
+
+  const isCreatingApiKey = useRef(false);
+
+  const apiKeyQuery = trpc.apiKey.getValidApiKey.useQuery(
+    { isShareId },
+    {
+      refetchOnWindowFocus: false,
+    },
+  );
+
+  const createApiKeyMutation = trpc.apiKey.createDefaultApiKey.useMutation({
+    onSuccess: () => {
+      apiKeyQuery.refetch();
+      isCreatingApiKey.current = false;
+      setCreationError(null);
+    },
+    onError: (error) => {
+      isCreatingApiKey.current = false;
+      setCreationError(error.message);
+      setIsInitializing(false);
+    },
+    onSettled: () => {
+      // Only update initialization state if we're no longer creating an API key
+      if (!isCreatingApiKey.current) {
+        setIsInitializing(false);
+      }
+    },
+  });
+
+  useEffect(() => {
+    if (!apiKeyQuery.isLoading) {
+      if (!apiKeyQuery.data && !isCreatingApiKey.current && isCreateAutoKey) {
+        isCreatingApiKey.current = true;
+        createApiKeyMutation.mutate();
+      }
+    }
+  }, [apiKeyQuery.isLoading, apiKeyQuery.data, isCreateAutoKey]);
+
+  return (
+    <APIContext.Provider
+      value={{
+        isInitializing,
+        isCreatingApiKey,
+        creationError,
+        apiKey: apiKeyQuery.data?.key || '',
+        isApiKeyLoading: apiKeyQuery.isLoading,
+      }}
+    >
+      {children}
+    </APIContext.Provider>
+  );
+};
+export const useAPIContext = () => {
+  const context = useContext(APIContext);
+  if (!context) {
+    throw new Error('setup must be used within a APIkeyProvider');
+  }
+  return context;
+};
